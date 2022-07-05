df79bfd2de7397011a8cada0a14b301a9d775824
Prabhashearee <prabhashwaree57@gmail.com>
2022-07-05 20:20:32 +0530

    Update jpaConfig,webConfig,WebRootConfig commit

--- BackEnd/src/main/java/lk/ijse/experdia_car_rental_system/config/WebAppConfig.java
+++ BackEnd/src/main/java/lk/ijse/experdia_car_rental_system/config/WebAppConfig.java
@@ -1,4 +1,20 @@
 package lk.ijse.experdia_car_rental_system.config;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@ComponentScan("lk.ijse.experdia_car_rental_system")
+@EnableWebMvc
+
 public class WebAppConfig {
+
+    @Bean
+    public ModelMapper modelMapper(){
+
+        return new ModelMapper();
+    }
 }
--- BackEnd/src/main/java/lk/ijse/experdia_car_rental_system/config/WebRootConfig.java
+++ BackEnd/src/main/java/lk/ijse/experdia_car_rental_system/config/WebRootConfig.java
@@ -1,4 +1,11 @@
 package lk.ijse.experdia_car_rental_system.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@Import(JPAConfig.class)
+
 public class WebRootConfig {
+
 }
